410450e29790ecbf6ed9b34601eeb4450a2ffd95
burzan85@googlemail.com <burzan85@googlemail.de>
2019-07-23 16:56:55 +0200

    start develop for client api

--- Makefile
+++ Makefile
@@ -58,8 +58,8 @@ CMAKE_BINARY_DIR = /Users/cefour/Projects/pmq
 
 # Special rule for the target edit_cache
 edit_cache:
-	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --cyan "Running CMake cache editor..."
-	/usr/local/Cellar/cmake/3.11.2/bin/ccmake -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
+	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --cyan "No interactive CMake dialog available..."
+	"/Users/cefour/Library/Application Support/JetBrains/Toolbox/apps/CLion/ch-0/191.7479.33/CLion.app/Contents/bin/cmake/mac/bin/cmake" -E echo No\ interactive\ CMake\ dialog\ available.
 .PHONY : edit_cache
 
 # Special rule for the target edit_cache
---
+++ client/messenger.hpp
@@ -0,0 +1,71 @@
+//
+// Created by pmqtt on 2019-07-23.
+//
+
+#ifndef PMQ_MESSENGER_HPP
+#define PMQ_MESSENGER_HPP
+#include <string>
+#include <pair>
+namespace pmq::client{
+
+    /*HOW TO USE
+     *
+     * int main(int argc,char **argv){
+     *  pmq::client::messenger messenger;
+     *  messenger
+     *      .set_broker_address("127.0.0.1",1883)
+     *      .set_qos(1)
+     *      .set_clean_session(true)
+     *      .set_keep_alive(2);
+     *
+     *  messenger.subscribe("first/topic1");
+     *
+     *  messenger.publish("first/topic1","My first message");
+     *  // blocked till received message from broker or a disconnect event was send
+     *  std::pair<std::string,std::string> msg =  messenger.get_message();
+     *  std::cout<<"Topic: "<< msg.first << "Payload:"<<msg.second<<std::endl;
+     *
+     *
+     * }
+     *
+     */
+
+    class messenger{
+    public:
+        messenger();
+
+        //Fluent interface
+        /*====================*/
+        /**
+         * @brief sets the qualaty of service for publish and subscribe
+         * @param qos valid qos is 0,1 and 2
+         * @throws qos_exception qos_exception will be thrown if qos > 2
+         * @return messenger
+         */
+        messenger & set_qos(const std::size_t qos);
+        /**
+         *
+         * @param address hostadress,ip4 or ipv6 adresse of the broker
+         * @param port port number of the broker
+         * @return messenger
+         */
+        messenger & set_broker_address(const std::string & address ,std::size_t port);
+        /**
+         *
+         * @param address adress in mqtt uri style e.g mqtt://host:1883
+         * throws uri_format_exception uri_format_exception will be thrown if address ill formated uri
+         * @return messenger
+         */
+        messenger & set_broker_address(const std::string & address);
+
+        messenger & set_keep_alive(std::size_t sec);
+        messenger & set_clean_session(bool clean);
+
+        void subscribe(const std::string & topic);
+        void publish(const std::string & topic,const std::string & payload);
+        std::pair<std::string,std::string> get_message();
+
+    };
+}
+
+#endif //PMQ_MESSENGER_HPP
