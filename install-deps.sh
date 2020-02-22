cdc24018e2416e42fba88954d0a0db6df6a39897
Michael Burzan <michaelburzan@Michaels-MacBook-Pro.local>
2020-02-22 22:30:16 +0100

    load websocketpp from source and compile

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get remove --auto-remove gcc
 sudo apt-get remove --auto-remove g++
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt-get search boost && sudo apt-get install gcc-8 g++-8
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt-cache search boost && sudo apt-get install gcc-8 g++-8
 sudo add-apt-repository ppa:mhier/libboost-latest && sudo apt update &&sudo apt install libboost-dev
 sudo apt-get install libssl-dev cmake g++
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
